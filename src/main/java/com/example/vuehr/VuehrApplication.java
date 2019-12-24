18162c378d34728ade173163cdf80bc3555fba59
xs38077 <406896045@qq.com>
2019-12-24 15:55:33 +0800

    V0.6

--- src/main/java/com/example/vuehr/VuehrApplication.java
+++ src/main/java/com/example/vuehr/VuehrApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+/**
+ * branch: master v0.6
+ */
 @SpringBootApplication
 @EnableCaching
 public class VuehrApplication {
